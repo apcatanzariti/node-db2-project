084ca3aa842d967ea0ea6d05f4a9a1bc1c469e50
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-16 17:58:57 -0400

    finished with MVP

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,29 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Cars = require('./cars-model');
+const router = require('express').Router();
+const { checkCarId, checkCarPayload, checkVinNumberUnique } = require('./cars-middleware');
+
+module.exports = router;
+
+router.get('/', (req, res) => {
+    Cars.getAll()
+    .then(cars => {
+        res.status(200).json(cars);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'something went wrong fetching all cars' });
+    })
+});
+
+router.get('/:id', checkCarId, (req, res) => {
+    res.json(req.car);
+});
+
+router.post('/', checkCarPayload, checkVinNumberUnique, async (req, res) => {
+    try {
+        const data = await Cars.create(req.body);
+        res.status(200).json(data);
+    } catch {
+        res.status(500).json({ message: 'something went wrong adding this car' });
+    }
+});
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,20 @@
 // DO YOUR MAGIC
+exports.up = function(knex) {
+    // does the structural changes to the db
+    return knex.schema
+      .createTable('cars', table  => {
+          table.increments('car_id');
+          table.text('vin').unique().notNullable();
+          table.text('make').notNullable();
+          table.text('model').notNullable();
+          table.decimal('mileage').notNullable();
+          table.text('car_title');
+          table.text('transmission');
+      })
+  };
+  
+  exports.down = function(knex) {
+    // undoes those changes
+    // the table drops should occur in the OPPOSITE order as they are created above. First created, last dropped.
+    return knex.schema.dropTableIfExists('cars');
+  };
